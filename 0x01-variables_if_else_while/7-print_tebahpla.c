60d296a636e0f9a94ea86e931f866ea7067bb8f3
shantel <ogbuagushantel@gmail.com>
2023-02-19 14:38:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - a program that prints the alphabet in lowercase, the reverse
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x = 'Z';
+
+	for (; x >= 'A'; x--)
+	{
+		putchar (tolower(x));
+	}
+	putchar ('\n');
+
+	return (0);
