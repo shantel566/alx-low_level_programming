a24c42869387abec1512962f5dbeae976005a54e
shantel <ogbuagushantel@gmail.com>
2023-02-19 15:16:24 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - use putcher to prints all single digit numbers of base 16
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
+		8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+		putchar(number[n]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
