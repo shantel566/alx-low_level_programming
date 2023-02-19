9f9543bf36f5be1fe49a50071abf5c3906fc22ec
shantel <ogbuagushantel@gmail.com>
2023-02-19 14:19:39 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - use putcher to prints all single digit numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number [10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(number[n]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
