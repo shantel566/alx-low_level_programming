715d18d2290f47dfd901dc45df69031fb323b71b
shantel <ogbuagushantel@gmail.com>
2023-02-19 16:09:46 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - use putcher to prints all possible single digit numbers of base
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number [10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(number[n]);
+	}
+
+		if (number[n] != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
