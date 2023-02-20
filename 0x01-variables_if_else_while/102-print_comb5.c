6094b447f3cc99afd4c564bb61193beaafda0287
shantel <ogbuagushantel@gmail.com>
2023-02-19 18:26:38 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		for (n2 = n1 + 1; n2 <= 99; n2++)
 		{
 			putchar((n1 / 10) + '0');
-			putchar((n2 % 10) + '0');
+			putchar((n1 % 10) + '0');
 			putchar(' ');
 			putchar((n2 / 10) + '0');
 			putchar((n2 % 10) + '0');
