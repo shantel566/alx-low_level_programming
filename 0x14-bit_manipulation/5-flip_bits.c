e4fe0ad01e6d32377b769b2f3a740a8413aeaf9e
Shantel566 <ogbuagushantel@gmail.com>
2023-04-10 16:02:05 -0700

    maniulate

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,9 +11,9 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m, bits = 64;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	while (xor > 64)
+	while (xor > 0)
 	{
 		bits += (xor & 1);
 		xor >>= 1;
